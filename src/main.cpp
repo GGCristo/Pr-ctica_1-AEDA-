f8ea57454a064e7fafcced6e71d5ad4474c8924e
Cristo <cgg.code@gmail.com>
2020-02-12 15:11:18 +0000

    makefile básico y arreglos menores

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-#include <dll_node_t.hpp>
-#include <dll_t.hpp>
-#include <stack_l_t.hpp>
-#include <stack_v_t.hpp>
-#include <vector_t.hpp>
+#include "../include/dll_node_t.hpp"
+#include "../include/dll_t.hpp"
+#include "../include/stack_l_t.hpp"
+#include "../include/stack_v_t.hpp"
+#include "../include/vector_t.hpp"
 using namespace AEDA;
 
 int main(){
